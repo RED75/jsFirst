aee71742d9613cbdc4439d26961d6e0b92a190fb
RED75 <Aleksei_rul@mail.ru>
2016-05-25 17:22:51 +0600

    2:added while

--- battleSheep.js
+++ battleSheep.js
@@ -1,3 +1,14 @@
 /**
  * Created by REDWork on 25.05.2016.
  */
+var location1;
+var location2;
+var location3;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+//Todo:
+while (prompt("hi")!="a"){}
