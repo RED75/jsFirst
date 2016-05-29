dd966056eea76cc0c9f605717f3632585d023d47
Aleksei Kornilov <aleksei_rul@mail.ru>
2016-05-29 23:15:34 +0600

    2d

--- battleSheep.js
+++ battleSheep.js
@@ -1,6 +1,10 @@
 /**
  * Created by REDWork on 25.05.2016.
  */
+function inc(val)
+{
+    val++;
+}
 var location1;
 var location2;
 var location3;
@@ -8,7 +12,28 @@ var location3;
 var guess;
 var hits = 0;
 var guesses = 0;
+//
+//var isSunk = false;
+////Todo:
+////while (prompt(Math.random())!="a"){}
+//inc(hits)
+//alert(hits);
 
-var isSunk = false;
-//Todo:
-while (prompt("hi")!="a"){}
+var textStiler
+{
+     function SetRedColor(id){
+        var textField=document.getElementById(id);
+         if(textField!=null)
+             textField.setAttribute("class","redtext");
+    }
+}
+function SetRedColorFunc(){
+    var textField=document.getElementById("mainHead");
+    if(textField!=null)
+    {
+        if(textField.getAttribute("class")=="redText")
+            textField.setAttribute("class","blackText");
+        else
+            textField.setAttribute("class","redText");
+    }
+}
---
+++ battleSheep2D.js
@@ -0,0 +1,13 @@
+/**
+ * Created by RED75 on 29.05.2016.
+ */
+function placeMiss() {
+    if(document.getElementById("guessInput").innerHTML=="A0")
+    {
+        var cell= document.getElementById("00");
+        cell.setAttribute("class", "hit");
+
+    }
+    alert(document.getElementById("guessInput").innerHTML.toString());
+
+}
