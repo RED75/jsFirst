8ecf7af1d5f29a4ed37f15f4aa4a2796f72aef46
Aleksei Kornilov <aleksei_rul@mail.ru>
2016-05-30 11:12:25 +0600

    2d mvc added

--- battleSheep2D.js
+++ battleSheep2D.js
@@ -1,14 +1,39 @@
 /**
  * Created by RED75 on 29.05.2016.
  */
-function placeMiss() {
-    if(document.getElementById("guessInput").innerHTML=="A0")
-    {
-        var cell= document.getElementById("00");
-        cell.setAttribute("class", "hit");
-
+var view = {
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById("messageArea");
+        if (messageArea)
+            messageArea.innerHTML = msg;
+        else console.log("messageArea hasn't been found");
+    },
+    displayHit: function (location) {
+        var cell = document.getElementById(location);
+        if(cell)
+            cell.setAttribute("class", "hit");
+        else console.log("cell hasn't been found");
+    },
+    displayMiss: function (location) {
+        var cell = document.getElementById(location);
+        if(cell)
+            cell.setAttribute("class", "miss");
+        else console.log("cell hasn't been found");
     }
-    alert(document.getElementById("guessInput").innerHTML.toString());
-    //todo: page 354
+};
+var model = {};
+var controller = {};
 
-}
+var testView = {
+    startTest: function(){
+        view.displayMiss("00");
+        view.displayHit("34");
+        view.displayMiss("55");
+        view.displayHit("12");
+        view.displayMiss("25");
+        view.displayHit("26");
+        view.displayMessage("Tap tap, is this thing on?");
+    }
+
+
+};
